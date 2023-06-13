b42c0340fd10c6724e0eab9388136d568724ebae
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-06-13 10:22:33 -0400

    add dom/index and dom/manipulation

---
+++ js/dom/manipulation.js
@@ -0,0 +1,13 @@
+
+
+const chapter1 = document.getElementById('1')
+
+const section11 = document.getElementById('1.1')
+
+const section11WithQuerySelector = chapter1.querySelector('.section')
+
+
+const allSections = document.querySelectorAll(".section")
+
+
+
