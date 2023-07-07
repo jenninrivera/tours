5679f97b420ac62e4972ff842a97cfa5b3d840ec
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-07-07 13:31:00 -0400

    refactor spell component

--- react/effect-example/src/App.js
+++ react/effect-example/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import QueryForm from "./components/QueryForm.js";
-import Spell from "./components/Spell.js"
+import Spell from "./components/Spell.js";
 function App() {
     const [spellList, setSpellList] = useState([]);
     const [spellInfo, setSpellInfo] = useState({});
@@ -13,22 +13,22 @@ function App() {
     }, []);
     function displayQuery(event) {
         event.preventDefault();
-        console.log(spellList)
-        const spell= spellList.find(spell =>{
-            return spell.name.toLowerCase()
-                             .includes(query.toLowerCase())
-        })
-        if(spell === undefined){
-            setSpellInfo({error:`${query}: spell not found`})
-        }else{
-        fetch(`https://www.dnd5eapi.co${spell.url}`)
-            .then((res) => res.json())
-            .then((data) => setSpellInfo(data));
+        console.log(spellList);
+        const spell = spellList.find((spell) => {
+            return spell.name.toLowerCase().includes(query.toLowerCase());
+        });
+        if (spell === undefined) {
+            setSpellInfo({ error: `${query}: spell not found` });
+        } else {
+            fetch(`https://www.dnd5eapi.co${spell.url}`)
+                .then((res) => res.json())
+                .then((data) => setSpellInfo(data));
         }
         event.target.reset();
     }
     return (
         <>
+            <h1>Spell Search</h1>
             <QueryForm
                 updateQuery={(event) => setQuery(event.target.value)}
                 displayQuery={displayQuery}
--- react/effect-example/src/components/Spell.js
+++ react/effect-example/src/components/Spell.js
@@ -1,15 +1,31 @@
-
-
-function Spell({spellInfo}){
-    return(
-        <>
-        <ul>
-            <li><strong>Name: </strong>{spellInfo.name}</li>
-            <li><strong>Range: </strong>{spellInfo.range}</li>
-            <li><strong>Description: </strong>{spellInfo.desc}</li>
-            {spellInfo?.error ? <li>Error: {spellInfo.error}</li> : <></>}
-        </ul>
-        </>
-    )
+function Spell({ spellInfo }) {
+    if (spellInfo.error !== undefined) {
+        return (
+            <>
+                <ul>
+                    <li>Error: {spellInfo.error}</li>
+                </ul>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <ul>
+                    <li>
+                        <strong>Name: </strong>
+                        {spellInfo.name}
+                    </li>
+                    <li>
+                        <strong>Range: </strong>
+                        {spellInfo.range}
+                    </li>
+                    <li>
+                        <strong>Description: </strong>
+                        {spellInfo.desc}
+                    </li>
+                </ul>
+            </>
+        );
+    }
 }
-export default Spell
+export default Spell;
