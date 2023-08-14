9453252fb6724cf2ea394cee022523cf35c30237
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-08-14 14:56:48 -0400

    solution for flask routes exercise

--- flask/routes/app.py
+++ flask/routes/app.py
@@ -4,46 +4,58 @@ import json
 app = Flask(__name__)
 
 
-
 @app.before_request
 def load_data():
-    with open('db.json') as f:
-        g.data = json.load(f)
+    with open("db.json") as f:
+        g.data: dict = json.load(f)
 
-@app.route('/')
+
+@app.route("/")
 def root():
     return "<h1>Welcome to the simple json server<h1>"
 
-@app.get('/langs')
+
+@app.get("/langs")
 def get_langs():
-    return make_response(jsonify(g.data['langs']), 200)
+    return make_response(jsonify(g.data["langs"]), 200)
 
-'''
-TODO: Add error handling if the id is not found
-'''
-@app.get('/langs/<int:id>')
-def get_lang_by_id(id:int):
-    lang = [l for l in g.data['langs'] if l['id'] == id][0]
-    return make_response(jsonify(lang), 200)
 
-'''
-TODO: Implement a POST request
-Note that in order to persist the change you will have 
-to write it to db.json. The route and function have been
-provided. You'll also have to take care of keeping track
-of giving the new item an id.
-'''
-@app.post('/langs')
+
+
+@app.get("/langs/<int:id>")
+def get_lang_by_id(id: int):
+    lang_list: list[dict] = [l for l in g.data["langs"] if l["id"] == id]
+    if len(lang_list) == 0:
+        return make_response(jsonify({"error": f"id {id} not found"}), 404)
+    return make_response(jsonify(lang_list[0]), 200)
+
+
+
+
+@app.post("/langs")
 def post_lang():
-    return ''
+    new_lang: dict = request.get_json()
+    max_id: int = max([item["id"] for item in g.data["langs"]])
+    new_lang["id"] = max_id + 1
+    g.data["langs"].append(new_lang)
+    print(g.data)
+    with open("db.json", "w") as f:
+        json.dump(g.data, f, indent=4)
+    return make_response(jsonify(new_lang), 201)
 
-'''
-TODO: Implement a DELETE request
-Note that in order to persist the change you will have 
-to write it to db.json. You'll have to write the 
-route and function.
-'''
 
-if __name__ == '__main__':
+
+
+@app.delete("/langs/<int:id>")
+def delete_lang_by_id(id: int):
+    updated_langs: list[dict] = [item for item in g.data["langs"] if item["id"] != id]
+    if len(updated_langs) == len(g.data["langs"]):
+        return make_response(jsonify({"error": f"id {id} not found"}), 404)
+    g.data["langs"] = updated_langs
+    with open("db.json", "w") as f:
+        json.dump(g.data, f, indent=4)
+    return make_response(jsonify({}), 200)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
--- flask/routes/request.sh
+++ flask/routes/request.sh
@@ -9,9 +9,21 @@
 #     -i, --include - Include the response headers.
 #     -d, --data - The data to be sent.
 #     -H, --header - Additional header to be sent.
+method=$1
 
-curl -X GET localhost:5555/langs
+if [ $method == "get" ]
+then
+    curl -X GET localhost:5555/langs
+fi
 
-# curl -X POST -H "Content-Type:application/json" \
-# -d {"name":"test", "creator":"test", "year":9999, "users":0} \
-# localhost:5555/langs
+if [ $method == 'post' ]
+then
+    curl -X POST -H "Content-Type:application/json" \
+    -d '{"name":"ML", "creator":"Milner", "year":1973, "users":0}' \
+    localhost:5555/langs
+fi
+
+if [ $method == 'delete' ]
+then
+    curl -X DELETE localhost:5555/langs/7
+fi
