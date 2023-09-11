72c7957e5ed99bd3e9d10f4f19fc7f2db0b4d208
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-09-11 10:24:25 -0400

    make starter/solution code for flask routes

---
+++ flask/routes/starter/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, make_response, jsonify, request, g
+import json
+
+app = Flask(__name__)
+
+
+@app.before_request
+def load_data():
+    with open("db.json") as f:
+        g.data: dict = json.load(f)
+
+
+@app.after_request
+def save_data():
+   with open('db.json', 'w') as f:
+      json.dump(g.data, f, indent=4)
+
+@app.route("/")
+def root():
+    return "<h1>Welcome to the simple json server<h1>"
+
+#@app.route('/langs', methods=['GET'])
+@app.get("/langs")
+def get_langs():
+    pass
+
+
+
+
+@app.get("/langs/<int:id>")
+def get_lang_by_id(id: int):
+    pass
+
+
+
+
+# TODO: write post route
+
+# TODO: write delete route
+
+# TODO: write patch route
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
