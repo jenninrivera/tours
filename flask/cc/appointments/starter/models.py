f775c78088f7bbc057602855c267e53bf4f45ae8
Daniel Gaston <daniel.gaston@flatironschool.com>
2023-09-18 09:35:23 -0400

    move flask appointments example to starter/solution folders, update readme

---
+++ flask/cc/appointments/starter/models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+import string
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+db = SQLAlchemy(metadata=metadata)
+
+class Patient(db.Model, SerializerMixin):
+    __tablename__ = 'patient_table'
+    id = db.Column(db.Integer, primary_key=True)
+    
+class Appointment(db.Model, SerializerMixin):
+    __tablename__ = 'appointment_table'
+    id = db.Column(db.Integer, primary_key=True)
+    
+
+
+class Doctor(db.Model, SerializerMixin):
+    __tablename__ = 'doctor_table'
+
