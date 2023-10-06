959e1289e9504fb69889a95fe91047f3923dcf31
Jenni Rivera <jenninrivera@gmail.com>
2023-10-06 14:49:31 -0400

    Added solution for appointment cc

--- flask/cc/appointments/starter/app.py
+++ flask/cc/appointments/starter/app.py
@@ -20,7 +20,66 @@ db.init_app(app)
 def index():
     return "doctor/patient"
 
+@app.get('/doctors')
+def get_doctors():
+    doctors = Doctor.query.all()
+    data = [doctor.to_dict(rules=("-appointments",)) for doctor in doctors]
+    
+    return make_response(jsonify(data), 200)
 
+@app.get('/doctors/<int:id>')
+def doctor_by_id(id):
+    doctor = Doctor.query.filter(Doctor.id == id).first()
+    if not doctor:
+        return make_response(jsonify({"error":"doctor does not exist"}), 404)
+    return make_response(jsonify(doctor.to_dict()), 200)
+
+# @app.get('/patients/<int:id>')
+# def patient_by_id(id):
+#     patient = Patient.query.filter(Patient.id == id).first()
+#     if not patient:
+#         return make_response(jsonify({"error":"patient does not"}), 404)
+#     doctor_list = Patient.doctor.query.all()
+#     return make_response(jsonify(patient.to_dict(rules=("-appointments",))), 200)
+
+@app.post('/doctors')
+def post_doctor():
+    data = request.json
+    new_doctor = Doctor(name=data['name'], specialty=data['specialty'])
+    db.session.add(new_doctor)
+    db.session.commit()
+    return make_response(jsonify(new_doctor.to_dict()), 201)
+
+@app.post('/appointments')
+def post_appointments():
+    data = request.json
+    new_appiontment = Appointment(day=data['day'], doctor_id=data['doctor_id'], patient_id=data['patient_id'])
+    db.session.add(new_appiontment)
+    db.session.commit()
+    return make_response(jsonify(new_appiontment.to_dict(rules=('-day', '-doctor_id', '-patient_id',))), 201)
+
+@app.delete('/appointments/<int:id>')
+def delete_appointment(id):
+    appointment = Appointment.query.filter(Appointment.id == id).first()
+    db.session.delete(appointment)
+    db.session.commit()
+    return make_response(jsonify({}), 200)
+
+@app.patch('/patients/<int:id>')
+def patch_patient(id):
+    patient = Patient.query.filter(Patient.id == id).first()
+    if not patient:
+        return make_response({"error":"patient does not exist"}, 404)
+    data = request.json
+    try:
+        for key in data:
+            setattr(patient, key, data[key])
+        db.session.add(patient)
+        db.session.commit()
+        return make_response(jsonify(patient.to_dict(rules=("-appointments",))), 201)
+    except ValueError as e:
+        print(e)
+        return make_response(jsonify({"error":"bad request"}), 404)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- flask/cc/appointments/starter/models.py
+++ flask/cc/appointments/starter/models.py
@@ -15,18 +15,46 @@ db = SQLAlchemy(metadata=metadata)
 
 class Patient(db.Model, SerializerMixin):
     __tablename__ = "patient_table"
+    serialize_rules = ("-appointments.patient",)
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
 
+    appointments = db.relationship("Appointment", back_populates="patient", cascade=("all, delete"))
+    doctor = association_proxy("appointments", "doctor")
 
 class Appointment(db.Model, SerializerMixin):
     __tablename__ = "appointment_table"
+    serialize_rules = ("-patient.appointments", "-doctor.appointments",)
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String, nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey("doctor_table.id"), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey("patient_table.id"), nullable=False)
 
 
     id = db.Column(db.Integer, primary_key=True)
+
+    patient = db.relationship("Patient", back_populates="appointments")
+    doctor = db.relationship("Doctor", back_populates="appointments")
+
+    @validates('day')
+    def validates_day(self, key, day):
+        if (day == "Saturday") or (day == "Sunday"):
+            raise ValueError("Day must be between Monday and Friday")
+        return day
 
 
 class Doctor(db.Model, SerializerMixin):
     __tablename__ = "doctor_table"
-
+    serialize_rules = ("-appointments.doctor",)
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    specialty = db.Column(db.String, nullable=False)
 
+    appointments = db.relationship('Appointment', back_populates='doctor', cascade=('all, delete'))
+    patient = association_proxy("appointments", "patient")
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name.startswith("Dr."):
+            raise ValueError("Doctor name must start with Dr.")
+        return name
