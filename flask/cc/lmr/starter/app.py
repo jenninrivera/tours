f9392a04da56da6a31b0bb000ccd6d87fbb64916
Jenni Rivera <jenninrivera@gmail.com>
2023-10-11 15:37:55 -0400

    completed lmr cc

--- flask/cc/lmr/starter/app.py
+++ flask/cc/lmr/starter/app.py
@@ -20,6 +20,52 @@ db.init_app(app)
 def index():
     return "right/left"
 
+@app.get("/rights")
+def get_rights():
+    rights = Right.query.all()
+    data = [right.to_dict(rules=("-middles",)) for right in rights]
+    return make_response(jsonify(data), 200)
 
+@app.get("/rights/<int:id>")
+def get_right_by_id(id):
+    right = Right.query.filter(Right.id == id).first()
+    if not right:
+        return make_response(jsonify({"error":"right does not exist"}))
+    return make_response(jsonify(right.to_dict()), 200)
+
+@app.post("/lefts")
+def post_left():
+    data = request.json
+    try:
+        new_left = Left(column=data.get("column"))
+        db.session.add(new_left)
+        db.session.commit()
+        return make_response(jsonify(new_left.to_dict()), 200)
+    except:
+        return make_response(jsonify({"error":"cannot add left"}), 404)
+
+@app.patch("/middles/<int:id>")
+def patch_middle(id):
+    middle_patch = db.session.get(Middle, id)
+    if not middle_patch:
+        return make_response(jsonify({"error":"middle does not exist"}), 404)
+    data = request.json
+    try:
+        for key in data:
+            setattr(middle_patch, key, data[key])
+            db.session.add(middle_patch)
+            db.session.commit()
+            return make_response(jsonify(middle_patch.to_dict(rules=("-left", "-right",))), 200)
+    except:
+        return make_response(jsonify({"error":"could not update middle"}), 404)
+
+@app.delete("/lefts/<int:id>")
+def delete_left(id):
+    left = Left.query.filter(Left.id == id).first()
+    if not left:
+        return make_response(jsonify({"eeror":"Left does not exist"}), 404)
+    db.session.delete(left)
+    db.session.commit()
+    return make_response(jsonify({}), 200)
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- flask/cc/lmr/starter/models.py
+++ flask/cc/lmr/starter/models.py
@@ -16,17 +16,50 @@ db = SQLAlchemy(metadata=metadata)
 
 class Left(db.Model, SerializerMixin):
     __tablename__ = "left_table"
-
+    serialize_rules = ("-middles.left",)
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable=False, unique=True)
 
+    middles = db.relationship("Middle", back_populates="left")
+    rights = association_proxy("middles", "right")
+
+    @validates("column")
+    def validate_column(self, key, column):
+        if not column.isalpha():
+            raise ValueError({"error":"Column must be letters only"})
+        return column
 
 class Middle(db.Model, SerializerMixin):
     __tablename__ = "middle_table"
+    serialize_rules = ("-left.middles", "-right.middles",)
 
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable=False)
+    left_id = db.Column(db.Integer, db.ForeignKey("left_table.id"), nullable=False)
+    right_id = db.Column(db.Integer, db.ForeignKey("right_table.id"), nullable=False)
 
+    left = db.relationship("Left", back_populates="middles")
+    right = db.relationship("Right", back_populates="middles")
+
+    @validates("column")
+    def validate_column(self, key, column):
+        for i in column:
+            if not i in string.punctuation:
+                raise ValueError({"error":"Column must be punctuation only"})
+        return column
+        
 
 class Right(db.Model, SerializerMixin):
     __tablename__ = "right_table"
-
+    serialize_rules =("-middles.right",)
     id = db.Column(db.Integer, primary_key=True)
+    column = db.Column(db.String, nullable=False, unique=True)
+
+    middles = db.relationship("Middle", back_populates="right")
+    lefts = association_proxy("middles", "left")
+
+    @validates("column")
+    def validate_column(self, key, column):
+        if not column.isdigit():
+            raise ValueError({"error":"Column must be digits only"})
+        return column
